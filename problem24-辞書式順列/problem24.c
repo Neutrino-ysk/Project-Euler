8146a4f5ee725e15063b224b0648a0add1aaf34d
Neutrino-ysk <q237051y@mail.kyutech.jp>
2021-05-12 02:07:19 +0900

    全体完成（解答が間違っているため調整が必要）

--- problem24-辞書式順列/problem24.c
+++ problem24-辞書式順列/problem24.c
@@ -2,44 +2,59 @@
 #define ASK_ORDER_NUM 1000000
 #define MAXNUM 9
 
+long long int digit[MAXNUM + 1];
+long long int num[MAXNUM + 1];
+long long int orderNum = 0;
+
+long long int makeFactorial(long long int num)
+{
+    if(num == 1) return 1;
+
+    return num * makeFactorial(num - 1);
+}
+
+void printDigit()
+{
+    for(long long int i = 0 ; i < MAXNUM + 1 ; i++){
+        printf("%lld", digit[i]);
+    }
+}
+
 int main(void)
 {
-    long long int digit[MAXNUM + 1];
-    long long int digitOrderNum = 1;
-    long long int orderNum = 0;
+    long long int digitCount;
 
-    for(int i = 0 ; i < MAXNUM + 1 ; i++){
+    for(long long int i = 0 ; i < MAXNUM + 1 ; i++){
         digit[i] = -1;
+        num[i] = i;
     }
 
-    for(long long int j = 0 ; j < MAXNUM + 1 ; j++){
-        for(long long int i = 0 ; i < MAXNUM + 1 ; i++){
-
-            for(long long int h = 0 ; h < MAXNUM + 1 ; h++){
-                if(digit[h] == i) i = MAXNUM + 1;
-            }
-
-            digitOrderNum = (MAXNUM + 1 - i) * digitOrderNum;
-
-            if(digitOrderNum + orderNum > ASK_ORDER_NUM || digitOrderNum + orderNum == ASK_ORDER_NUM){
-                digit[j] = i - 1;
-                orderNum = digitOrderNum / (MAXNUM + 1 - i) + orderNum;
-                i = MAXNUM + 1;
+    for(long long int dNum = 0 ; dNum < MAXNUM ; dNum++){
+        digitCount = -1;
+        while(orderNum < ASK_ORDER_NUM){
+            orderNum = makeFactorial(MAXNUM - dNum) + orderNum;
+            digitCount++;
+            while(num[digitCount] == -1){
+                digitCount++;
             }
         }
-    }
-    
+        orderNum = orderNum - makeFactorial(MAXNUM - dNum);
+        digitCount = digitCount - 1;
 
-    printf("%lld", digit[0]);
-    printf("%lld", digit[1]);
-    printf("%lld", digit[2]);
-    printf("%lld", digit[3]);
-    printf("%lld", digit[4]);
-    printf("%lld", digit[5]);
-    printf("%lld", digit[6]);
-    printf("%lld", digit[7]);
-    printf("%lld", digit[8]);
-    printf("%lld", digit[9]);
+        while(num[digitCount] == -1){
+            digitCount--;
+        }
+        digit[dNum] = num[digitCount];
+        num[digitCount] = -1;        
+    }
+
+    digitCount = 0;
+
+    while (num[digitCount] == -1) digitCount++;
+
+    digit[MAXNUM] = num[digitCount];
+
+    printDigit();
 
     return 0;
 }
