26639f8e89b51c8adcabb1b3ea49224401ccba14
Neutrino-ysk <q237051y@mail.kyutech.jp>
2021-05-18 00:16:53 +0900

    division関数の修正（途中）

--- problem26-逆数の循環節 その1/problem26.c
+++ problem26-逆数の循環節 その1/problem26.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
+#define MAX_NUM 1000
 
 int dCount = 0;
-int fBunsi;
-int printTrue = 1;
+int firstAmari;
+int printTrue = 0;
 
 void division(int bunsi, int bunbo)
 {   
     int amari = bunsi % bunbo;
     if(printTrue == 1) printf("%d", bunsi / bunbo);
     if(dCount == 0){
-        fBunsi = bunsi;
+        firstAmari = amari;
         if(printTrue == 1) printf(".");
     }
-    if(dCount != 0 && amari == fBunsi) amari = 0;
+    if(dCount != 0 && amari == firstAmari) amari = 0;
     dCount++;
     if(amari != 0) division((amari) * 10, bunbo);
 }
 
 int main(void)
 {
-    division(1, 11);
-    printf("\nD(p) = %d", dCount - 1);
+    int bunbo = MAX_NUM;
+    
+    /*while (bunbo != dCount)
+    {
+        dCount = 0;
+        printf("1 / %d = ", bunbo);
+        division(1, bunbo);
+        printf("\nD(p) = %d\n", dCount - 1);
+        bunbo--;
+    }*/
+    bunbo = 998;
+    printf("1 / %d = ", bunbo);
+    division(1, bunbo);
+    //printf("%d", bunbo);
 
     return 0;
 }
