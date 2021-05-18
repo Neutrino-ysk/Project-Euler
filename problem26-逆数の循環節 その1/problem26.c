8e98ea398527582379d6f638f65546fb5753641c
Neutrino-ysk <q237051y@mail.kyutech.jp>
2021-05-19 00:39:25 +0900

    素数の逆数の循環小数を導出する機能の完成->ver1.0

--- problem26-逆数の循環節 その1/problem26.c
+++ problem26-逆数の循環節 その1/problem26.c
@@ -3,12 +3,28 @@
 
 int dCount = 0;
 int firstAmari;
-int printTrue = 0;
+int printTrue = 1;
+
+int prime_disc(long int i)
+{
+  long int j = 2;
+
+  if(j <= i){
+    while(i % j != 0){
+      j++;
+    }
+  }
+  if(i == j){
+    return i;
+  }
+  return -1;
+}
 
 void division(int bunsi, int bunbo)
-{   
+{
+    int shou = bunsi / bunbo;
     int amari = bunsi % bunbo;
-    if(printTrue == 1) printf("%d", bunsi / bunbo);
+    if(printTrue == 1) printf("%d", shou);
     if(dCount == 0){
         firstAmari = amari;
         if(printTrue == 1) printf(".");
@@ -22,18 +38,18 @@ int main(void)
 {
     int bunbo = MAX_NUM;
     
-    /*while (bunbo != dCount)
+    while (bunbo != dCount - 1)
     {
         dCount = 0;
-        printf("1 / %d = ", bunbo);
-        division(1, bunbo);
-        printf("\nD(p) = %d\n", dCount - 1);
+        if(prime_disc(bunbo) != -1){
+            printf("1 / %d = ", bunbo);
+            division(1, bunbo);
+            printf("\nD(p) = %d\n", dCount - 1);
+        }        
         bunbo--;
-    }*/
-    bunbo = 998;
-    printf("1 / %d = ", bunbo);
-    division(1, bunbo);
-    //printf("%d", bunbo);
+    }
+    bunbo = bunbo + 1;
+    printf("result = %d", bunbo);
 
     return 0;
 }
