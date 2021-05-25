e888a32acd17e1caf70425c38e0e5a02b9d19523
Neutrino-ysk <q237051y@mail.kyutech.jp>
2021-05-26 00:48:06 +0900

    初期状態

--- problem27-二次式素数/problem27.c
+++ problem27-二次式素数/problem27.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int prime[];
+
 int prime_disc(int i)
 {
   int j = 2;
@@ -17,12 +19,15 @@ int prime_disc(int i)
 
 void setPrimeNum()
 {
-    int prime;
     int count = 1;
 
     for(int i = 1 ; i < 1001 ; i++){
         prime = prime_disc(i);
         if(prime != -1) count++;
+    }
+
+    for(int i = 0 ; i < count ; i++){
+
     }
 }
 
