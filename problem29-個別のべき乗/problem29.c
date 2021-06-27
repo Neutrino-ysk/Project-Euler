39b87228801640e06204e88e7533199de74cc54d
Neutrino-ysk <q237051y@mail.kyutech.jp>
2021-06-27 23:27:13 +0900

    canSimplificationの再起部分の作成途中

--- problem29-個別のべき乗/problem29.c
+++ problem29-個別のべき乗/problem29.c
@@ -9,31 +9,45 @@
 #define B_MIN  2
 
 //指数を単純化する関数
-int canSimplification(double a, double b)
+double *canSimplification(double a, double b)
 {
+    double* re = NULL;
+    re = (double*)malloc(sizeof(double) * 2);
+
+    double* re2 = NULL;
     double dResult;
     int count = 2;
     double exp;
     int iResult;
 
+    re[0] = -1;
+    re[1] = -1;
+
     while(dResult > 2){
         exp = 1.0 / count;
         dResult = pow(a, exp);
         iResult = dResult;
+        if(iResult == dResult){
+            re[0] = iResult;
+            re[1] = count * b;
+            re2 = canSimplification(re[0], re[1]);
+
+            if(re2[0] == -1) return re;
+        }
         count++;
-        if(iResult == dResult) return iResult + b;
     }
-    return 0;
+    return re;
 }
 
 int main(void)
 {
-    double a = 8.0;
+    double a = 16.0;
     double b = 2;
+    double* re = NULL;
 
-    printf("a = %lf\n", a);
-    printf("b = %lf\n", b);
-    printf("result = %d", canSimplification(a, b));
+    printf("(%lf, %lf)\n", a, b);
+    re = canSimplification(a, b);
+    printf("(%lf, %lf)\n", re[0], re[1]);
     
     /*for(int b = B_MIN ; b < B_MAX + 1 ; b++){
         for(int a = A_MIN ; a < A_MAX + 1 ; a++){
@@ -44,6 +58,8 @@ int main(void)
             }
         }
     }*/
+
+    free(re);
 
     return 0;
 }
