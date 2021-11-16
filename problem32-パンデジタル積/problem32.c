d9cad0708e0f9ab2dc51b30e810810bb146ffb4f
Neutrino-ysk <q237051y@mail.kyutech.jp>
2021-11-16 17:47:05 +0900

    ver1.0

---
+++ problem32-パンデジタル積/problem32.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#define NOD 9
+#define DIGIT_MAX 999999999
+//#define SUM_NUM 45
+//#define MUL_NUM 362880
+
+int digit[10];
+/*int digitSum = 0;
+int digitMul = 1;
+int isZero = 0;*/
+
+void initDigit()
+{
+    for(int i = 0 ; i < NOD ; i++){
+        digit[i] = NOD - i;
+    }
+}
+
+void nextDigit()
+{
+    int addDigit = 1;
+
+    for(int i = 0 ; i < NOD ; i++){
+        if(digit[i] == 9 && addDigit == 1){
+            digit[i] = 0;
+            addDigit = 1;
+        }else{
+            digit[i] = digit[i] + addDigit;
+            addDigit = 0;
+        }        
+    }
+}
+
+int checkIsPandigital()
+{
+    for(int j = 0 ; j < NOD ; j++){
+        for(int i = 0 ; i < NOD ; i++){
+            if(i != j && digit[j] == digit[i] || digit[i] == 0) return 0;
+        }
+    }
+    return 1;
+}
+
+int expNum(int powNum)
+{
+    int result = 1;
+    for(int i = 0 ; i < powNum ; i++){
+        result = result * 10;
+    }
+    return result;
+}
+
+int convertDigitNum()
+{
+    int result = 0;
+
+    for(int i = 0 ; i < NOD ; i++){
+        result = digit[i] * expNum(i) + result;
+    }
+
+    return result;
+}
+
+int sumDigit()
+{
+    int result = 0;
+
+    for(int i = 0 ; i < NOD ; i++){
+        result = digit[i] + result;
+    }
+
+    return result;
+}
+
+void printDigit()
+{
+    for(int i = NOD - 1 ; i > -1 ; i--){
+        printf("%d", digit[i]);
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    initDigit();
+
+    while(convertDigitNum() < DIGIT_MAX){
+        if(sumDigit() == 45){
+            //printDigit();
+            if(checkIsPandigital() == 1); //printDigit();
+        }
+        //printf("checkIsPandigital = %d\n", checkIsPandigital());
+        nextDigit();
+    }
+
+    return 0;
+}
