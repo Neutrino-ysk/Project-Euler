40571f3dea5ac86bdfbbcba43bd778ec0764bae6
Neutrino-ysk <q237051y@mail.kyutech.jp>
2022-03-18 13:15:06 +0900

    順列生成関数（基礎部分）

--- problem32-パンデジタル積/problem32.c
+++ problem32-パンデジタル積/problem32.c
@@ -1,100 +1,48 @@
 #include <stdio.h>
-#define NOD 5
-#define DIGIT_MAX 99999
-#define SUM_DIGIT 34
+#define MAXNUM 3
 
-int digit[10];
-int oneDigit[1];
-int twoDigit[2];
-int threeDigit[3];
-int fourDigit[4];
-/*int digitSum = 0;
-int digitMul = 1;
-int isZero = 0;*/
+int pNum[MAXNUM][6][MAXNUM];
 
-void initDigit()
-{
-    for(int i = 0 ; i < NOD ; i++){
-        digit[i] = NOD - i;
+int fact(int fNum){
+    if(fNum != 1){
+        return fNum * fact(fNum - 1);
+    }else{
+        return 1;
     }
 }
 
-void nextDigit()
+//順列生成関数
+void makePermutation(int hNum)
 {
-    int addDigit = 1;
+    int hNumNext;
+    int sNum = 0;
 
-    for(int i = 0 ; i < NOD ; i++){
-        if(digit[i] == 9 && addDigit == 1){
-            digit[i] = 0;
-            addDigit = 1;
-        }else{
-            digit[i] = digit[i] + addDigit;
-            addDigit = 0;
-        }        
+    if(hNum < MAXNUM){
+        hNumNext = hNum + 1;
+        makePermutation(hNumNext);
     }
-}
 
-int checkIsPandigital()
-{
-    for(int j = 0 ; j < NOD ; j++){
-        for(int i = 0 ; i < NOD ; i++){
-            if(i != j && digit[j] == digit[i] || digit[i] == 0) return 0;
+    while(sNum + 1 <= fact(hNum + 1)){
+        int dNum = 0;
+        
+        while(dNum < hNum + 1){
+            pNum[hNum][sNum][dNum] = dNum + 1;
+            /*printf("%d ", hNum);
+            printf("%d ", sNum);
+            printf("%d, ", dNum);*/
+            printf("%d", pNum[hNum][sNum][dNum]);
+            dNum++;
         }
-    }
-    return 1;
-}
-
-int expNum(int powNum)
-{
-    int result = 1;
-    for(int i = 0 ; i < powNum ; i++){
-        result = result * 10;
-    }
-    return result;
-}
-
-int convertDigitNum()
-{
-    int result = 0;
-
-    for(int i = 0 ; i < NOD ; i++){
-        result = digit[i] * expNum(i) + result;
-    }
-
-    return result;
-}
-
-int sumDigit()
-{
-    int result = 0;
-
-    for(int i = 0 ; i < NOD ; i++){
-        result = digit[i] + result;
-    }
-
-    return result;
-}
-
-void printDigit()
-{
-    for(int i = NOD - 1 ; i > -1 ; i--){
-        printf("%d", digit[i]);
+        sNum++;
+        printf(" ");
     }
     printf("\n");
 }
 
+//main
 int main(void)
 {
-    initDigit();
-
-    while(convertDigitNum() < DIGIT_MAX){
-        if(sumDigit() == SUM_DIGIT){
-            //printDigit();
-            if(checkIsPandigital() == 1) printDigit();
-        }
-        //printf("checkIsPandigital = %d\n", checkIsPandigital());
-        nextDigit();
-    }
+    makePermutation(0);
 
     return 0;
 }
